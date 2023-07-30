8b49cb5100231077aacdc2605adc5685b329117e
MauroDuque <mauricio.duque.eje@gmail.com>
2023-07-30 12:56:10 -0400

    bounding reducer

--- src/GAS.cpp
+++ src/GAS.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
+
 const int buttonPin = 15; // Pin where the button is connected
-int previusState = 0;
+int previousState = LOW;
 int counter = 0;
-int buttonState = 0;
+int buttonState = LOW;
+unsigned long debounceDelay = 50; // Adjust this value as needed
 
 int get_pulse_gas() {
     return buttonState;
@@ -17,10 +19,22 @@ void setup_gas() {
 }
 
 void loop_gas() {
-    buttonState = digitalRead(buttonPin); // Read the current state of the button
-  
-    if(buttonState != previusState) {
-        previusState = buttonState;
-        counter++;
+    int reading = digitalRead(buttonPin); // Read the current state of the button
+
+    // Apply debounce logic
+    if (reading != previousState) {
+        unsigned long debounceTime = millis();
+        while ((millis() - debounceTime) < debounceDelay) {
+            reading = digitalRead(buttonPin);
+        }
     }
+
+    if (reading != buttonState) {
+        buttonState = reading;
+        if (buttonState == HIGH) {
+            counter++;
+        }
+    }
+
+    previousState = reading;
 }
